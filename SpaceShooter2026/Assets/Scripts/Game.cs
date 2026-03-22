7eb61a50c43729a841c3ba4356028e6444a89a62
Leinad42 <153773009+Leinad42@users.noreply.github.com>
2026-03-22 16:25:17 -0500

    Made spawn timing more random

--- SpaceShooter2026/Assets/Scripts/Game.cs
+++ SpaceShooter2026/Assets/Scripts/Game.cs
@@ -2,7 +2,6 @@ using UnityEngine;
 
 public class Game : MonoBehaviour {
   // set in inspector
-  public float enemySpawnDelay;
   public GameObject enemyPrefab;
   public GameObject powerupPrefab;
   public BoxCollider2D spawnRange;
@@ -11,14 +10,34 @@ public class Game : MonoBehaviour {
   public BoxCollider2D spawnRangeBottom;
   public UI ui;
 
+  // Spawn delay ranges
+  public float minEnemySpawnDelay = 3f;
+  public float maxEnemySpawnDelay = 10f;
+
   // private fields
   private float powerUpDelay;
-  private float enemySpawnTimer;
   private float powerupSpawnTimer;
+
+  private float rightEnemySpawnTimer;
+  private float leftEnemySpawnTimer;
+  private float topEnemySpawnTimer;
+  private float bottomEnemySpawnTimer;
+
+  private float rightSpawnDelay;
+  private float leftSpawnDelay;
+  private float topSpawnDelay;
+  private float bottomSpawnDelay;
+
+  
 
   private void Start() {
     powerUpDelay = Random.Range(5f, 10f);
     powerupSpawnTimer = 0;
+
+    rightSpawnDelay = Random.Range(minEnemySpawnDelay, maxEnemySpawnDelay);
+    leftSpawnDelay = Random.Range(minEnemySpawnDelay, maxEnemySpawnDelay);
+    topSpawnDelay = Random.Range(minEnemySpawnDelay, maxEnemySpawnDelay);
+    bottomSpawnDelay = Random.Range(minEnemySpawnDelay, maxEnemySpawnDelay);
   }
 
   private void SpawnEnemy() {
@@ -32,7 +51,7 @@ public class Game : MonoBehaviour {
   private void SpawnEnemyLeft() {
     Vector3 leftEnemySpawnPt = new Vector3(
         Random.Range(spawnRangeLeft.bounds.min.x, spawnRangeLeft.bounds.max.x),
-        Random.Range(spawnRangeLeft.bounds.min.y, spawnRange.bounds.max.y),
+        Random.Range(spawnRangeLeft.bounds.min.y, spawnRangeLeft.bounds.max.y),
         0);
     Instantiate(enemyPrefab, leftEnemySpawnPt, Quaternion.identity);
   }
@@ -40,7 +59,7 @@ public class Game : MonoBehaviour {
   private void SpawnEnemyTop() {
     Vector3 topEnemySpawnPt = new Vector3(
         Random.Range(spawnRangeTop.bounds.min.x, spawnRangeTop.bounds.max.x),
-        Random.Range(spawnRangeTop.bounds.min.y, spawnRange.bounds.max.y),
+        Random.Range(spawnRangeTop.bounds.min.y, spawnRangeTop.bounds.max.y),
         0);
     Instantiate(enemyPrefab, topEnemySpawnPt, Quaternion.identity);
   }
@@ -64,14 +83,36 @@ public class Game : MonoBehaviour {
       return;
     }
 
-    // check spawn enemy
-    enemySpawnTimer += Time.deltaTime;
-    if (enemySpawnTimer >= enemySpawnDelay) {
+    // Right spawn timer
+    rightEnemySpawnTimer += Time.deltaTime;
+    if (rightEnemySpawnTimer >= rightSpawnDelay) {
       SpawnEnemy();
+      rightEnemySpawnTimer = 0.0f;
+      rightSpawnDelay = Random.Range(minEnemySpawnDelay, maxEnemySpawnDelay);
+    }
+
+    // Left spawn timer
+    leftEnemySpawnTimer += Time.deltaTime;
+    if (leftEnemySpawnTimer >= leftSpawnDelay) {
       SpawnEnemyLeft();
+      leftEnemySpawnTimer = 0.0f;
+      leftSpawnDelay = Random.Range(minEnemySpawnDelay, maxEnemySpawnDelay);
+    }
+
+    // Top spawn timer
+    topEnemySpawnTimer += Time.deltaTime;
+    if (topEnemySpawnTimer >= topSpawnDelay) {
       SpawnEnemyTop();
-      SpawnEnemyBottom();
-      enemySpawnTimer = 0.0f;
+      topEnemySpawnTimer = 0.0f;
+      topSpawnDelay = Random.Range(minEnemySpawnDelay, maxEnemySpawnDelay);
+    }
+
+    // Bottom spawn timer
+    topEnemySpawnTimer += Time.deltaTime;
+    if (topEnemySpawnTimer >= topSpawnDelay) {
+      SpawnEnemyTop();
+      topEnemySpawnTimer = 0.0f;
+      topSpawnDelay = Random.Range(minEnemySpawnDelay, maxEnemySpawnDelay);
     }
 
     // check spawn powerup
