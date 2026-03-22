c11d58d19a32e46edbd127272af34b4e95b0bdac
Leinad42 <153773009+Leinad42@users.noreply.github.com>
2026-03-22 15:53:15 -0500

    Added 3 more enemy spawn pts

--- SpaceShooter2026/Assets/Scripts/Game.cs
+++ SpaceShooter2026/Assets/Scripts/Game.cs
@@ -6,6 +6,9 @@ public class Game : MonoBehaviour {
   public GameObject enemyPrefab;
   public GameObject powerupPrefab;
   public BoxCollider2D spawnRange;
+  public BoxCollider2D spawnRangeLeft;
+  public BoxCollider2D spawnRangeTop;
+  public BoxCollider2D spawnRangeBottom;
   public UI ui;
 
   // private fields
@@ -25,6 +28,30 @@ public class Game : MonoBehaviour {
         0);
     Instantiate(enemyPrefab, enemySpawnPt, Quaternion.identity);
   }
+
+  private void SpawnEnemyLeft() {
+    Vector3 leftEnemySpawnPt = new Vector3(
+        Random.Range(spawnRangeLeft.bounds.min.x, spawnRangeLeft.bounds.max.x),
+        Random.Range(spawnRangeLeft.bounds.min.y, spawnRange.bounds.max.y),
+        0);
+    Instantiate(enemyPrefab, leftEnemySpawnPt, Quaternion.identity);
+  }
+
+  private void SpawnEnemyTop() {
+    Vector3 topEnemySpawnPt = new Vector3(
+        Random.Range(spawnRangeTop.bounds.min.x, spawnRangeTop.bounds.max.x),
+        Random.Range(spawnRangeTop.bounds.min.y, spawnRange.bounds.max.y),
+        0);
+    Instantiate(enemyPrefab, topEnemySpawnPt, Quaternion.identity);
+  }
+
+  private void SpawnEnemyBottom() {
+    Vector3 bottomEnemySpawnPt = new Vector3(
+        Random.Range(spawnRangeBottom.bounds.min.x, spawnRangeBottom.bounds.max.x),
+        Random.Range(spawnRangeBottom.bounds.min.y, spawnRangeBottom.bounds.max.y),
+        0);
+    Instantiate(enemyPrefab, bottomEnemySpawnPt, Quaternion.identity);
+  }
   private void SpawnPowerup() {
     Vector3 powerupSpawnPt = new Vector3(
         Random.Range(spawnRange.bounds.min.x, spawnRange.bounds.max.x),
@@ -41,6 +68,9 @@ public class Game : MonoBehaviour {
     enemySpawnTimer += Time.deltaTime;
     if (enemySpawnTimer >= enemySpawnDelay) {
       SpawnEnemy();
+      SpawnEnemyLeft();
+      SpawnEnemyTop();
+      SpawnEnemyBottom();
       enemySpawnTimer = 0.0f;
     }
 
